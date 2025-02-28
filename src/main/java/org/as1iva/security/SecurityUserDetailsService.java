bb6168590d4329f1b3a3d8646773744dd2f5c258
Tim Fadeev <1mailforwork1@gmail.com>
2025-02-28 12:10:26 +0300

    feat(security): add `SecurityUserDetailsService`

---
+++ src/main/java/org/as1iva/security/SecurityUserDetailsService.java
@@ -0,0 +1,23 @@
+package org.as1iva.security;
+
+import lombok.RequiredArgsConstructor;
+import org.as1iva.entity.User;
+import org.as1iva.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new SecurityUserDetails(user);
+    }
+}
