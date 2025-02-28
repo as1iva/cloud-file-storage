6428f85097f4da84872658dae4316fd6f7e6a1b0
Tim Fadeev <1mailforwork1@gmail.com>
2025-02-28 12:10:17 +0300

    feat(security): add `SecurityUserDetails`

---
+++ src/main/java/org/as1iva/security/SecurityUserDetails.java
@@ -0,0 +1,29 @@
+package org.as1iva.security;
+
+import lombok.RequiredArgsConstructor;
+import org.as1iva.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class SecurityUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+}
