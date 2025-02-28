7237fc66481f2e61560ecca57600f68e75680f61
Tim Fadeev <1mailforwork1@gmail.com>
2025-02-28 12:08:12 +0300

    feat(entity): add `User`

---
+++ src/main/java/org/as1iva/entity/User.java
@@ -0,0 +1,25 @@
+package org.as1iva.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
