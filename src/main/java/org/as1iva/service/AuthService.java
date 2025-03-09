3db9e4b3990d7a21a03bb87b4c5f5070b6733a4f
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-09 19:19:50 +0300

    feat(service): add `signIn` method in `AuthService`

--- src/main/java/org/as1iva/service/AuthService.java
+++ src/main/java/org/as1iva/service/AuthService.java
@@ -1,11 +1,18 @@
 package org.as1iva.service;
 
 import lombok.RequiredArgsConstructor;
+import org.as1iva.dto.request.UserLoginRequestDto;
 import org.as1iva.dto.request.UserRegistrationRequestDto;
+import org.as1iva.dto.response.UserLoginResponseDto;
 import org.as1iva.dto.response.UserRegistrationResponseDto;
 import org.as1iva.entity.User;
+import org.as1iva.mapper.UserLoginMapper;
 import org.as1iva.mapper.UserRegistrationMapper;
 import org.as1iva.repository.UserRepository;
+import org.as1iva.security.SecurityUserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +26,8 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final AuthenticationManager authenticationManager;
+
     public UserRegistrationResponseDto signUp(UserRegistrationRequestDto userRegistrationRequestDto) {
         String encodedPassword = passwordEncoder.encode(userRegistrationRequestDto.password());
 
@@ -28,5 +37,22 @@ public class AuthService {
                 .build();
 
         return UserRegistrationMapper.INSTANCE.toDto(userRepository.save(user));
+    }
+
+    public UserLoginResponseDto signIn(UserLoginRequestDto userLoginRequestDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLoginRequestDto.username(),
+                        userLoginRequestDto.password()
+                )
+        );
+
+        SecurityUserDetails principal = (SecurityUserDetails) authentication.getPrincipal();
+
+        User userEntity = User.builder()
+                .username(principal.getUsername())
+                .build();
+
+        return UserLoginMapper.INSTANCE.toDto(userEntity);
     }
 }
