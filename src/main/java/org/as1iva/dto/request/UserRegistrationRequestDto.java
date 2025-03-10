7cae8e644699a1e3c770861c1b4d68e64d8903f6
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-10 17:16:38 +0300

    refactor(dto): remove `confirmPassword` from `UserRegistrationRequestDto`

--- src/main/java/org/as1iva/dto/request/UserRegistrationRequestDto.java
+++ src/main/java/org/as1iva/dto/request/UserRegistrationRequestDto.java
@@ -4,6 +4,6 @@ package org.as1iva.dto.request;
 import lombok.Builder;
 
 @Builder
-public record UserRegistrationRequestDto(String username, String password, String confirmPassword) {
+public record UserRegistrationRequestDto(String username, String password) {
 
 }
