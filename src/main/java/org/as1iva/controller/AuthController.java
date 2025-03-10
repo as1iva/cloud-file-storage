228499a4f14f99ec2b195655000b9ca8ed5dae41
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-10 17:13:42 +0300

    feat(controller): add username validation in `AuthController`

--- src/main/java/org/as1iva/controller/AuthController.java
+++ src/main/java/org/as1iva/controller/AuthController.java
@@ -5,8 +5,8 @@ import org.as1iva.dto.request.UserLoginRequestDto;
 import org.as1iva.dto.request.UserRegistrationRequestDto;
 import org.as1iva.dto.response.UserLoginResponseDto;
 import org.as1iva.dto.response.UserRegistrationResponseDto;
-import org.as1iva.entity.User;
 import org.as1iva.service.AuthService;
+import org.as1iva.util.ValidationUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +20,16 @@ public class AuthController {
 
     @PostMapping("/sign-up")
     public ResponseEntity<UserRegistrationResponseDto> signUp(@RequestBody UserRegistrationRequestDto user) {
+
+        ValidationUtil.checkUsername(user.username());
+
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signUp(user));
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<UserLoginResponseDto> signIn(@RequestBody UserLoginRequestDto user) {
+
+        ValidationUtil.checkUsername(user.username());
 
         return ResponseEntity.ok(authService.signIn(user));
     }
