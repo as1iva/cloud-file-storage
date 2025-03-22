eeef0904ed6386dbe14239d478a993cec90efe1e
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-22 21:35:43 +0300

    feat(util): add `PathUtil`

---
+++ src/main/java/org/as1iva/util/PathUtil.java
@@ -0,0 +1,50 @@
+package org.as1iva.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+
+    public String getUserPath(String path, Long userId) {
+
+        String defaultFolder = String.format("user-%d-files/", userId + 1);
+
+        return defaultFolder + path;
+    }
+
+    public String getFileName(String path) {
+
+        int lastIndex = path.lastIndexOf("/");
+
+        if (lastIndex != -1) {
+            return path.substring(lastIndex + 1);
+        } else {
+            return path;
+        }
+    }
+
+    public String getFileRoot(String path) {
+
+        int lastIndex = path.lastIndexOf("/");
+
+        if (lastIndex != -1) {
+            return path.substring(0, lastIndex + 1);
+        } else {
+            return "";
+        }
+    }
+
+    public String getDirectoryName(String path) {
+
+        String pathWithoutLastSlash = path.replaceAll("/+$", "");
+
+        return getFileName(pathWithoutLastSlash);
+    }
+
+    public String getDirectoryRoot(String path) {
+
+        String pathWithoutLastSlash = path.replaceAll("/+$", "");
+
+        return getFileRoot(pathWithoutLastSlash);
+    }
+}
