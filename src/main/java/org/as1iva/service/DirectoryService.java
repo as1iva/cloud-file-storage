839488ef1ec1a8fcf7a9a38cdac5325d3d33ae2d
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-23 14:00:06 +0300

    feat(service): add `DirectoryService`

---
+++ src/main/java/org/as1iva/service/DirectoryService.java
@@ -0,0 +1,35 @@
+package org.as1iva.service;
+
+import lombok.RequiredArgsConstructor;
+import org.as1iva.dto.response.ResourceResponseDto;
+import org.as1iva.exception.InternalServerException;
+import org.as1iva.util.PathUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DirectoryService {
+
+    private final MinioService minioService;
+
+    private final String DIRECTORY_TYPE = "DIRECTORY";
+
+    public ResourceResponseDto createDirectory(String path, Long userId) {
+        String completePath = PathUtil.getUserPath(path, userId);
+
+        try {
+            minioService.createEmptyDirectory(completePath);
+
+            return ResourceResponseDto.builder()
+                    .path(PathUtil.getDirectoryRoot(path))
+                    .name(PathUtil.getDirectoryName(path))
+                    .type(DIRECTORY_TYPE)
+                    .build();
+
+        } catch (Exception e) {
+            throw new InternalServerException();
+        }
+    }
+}
