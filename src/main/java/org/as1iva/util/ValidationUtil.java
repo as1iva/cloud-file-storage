e9ea9e88b734fb73dff633f1e2c475bf9ddddab8
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-24 12:48:04 +0300

    feat(util): add `checkPath` method in `ValidationUtil`

--- src/main/java/org/as1iva/util/ValidationUtil.java
+++ src/main/java/org/as1iva/util/ValidationUtil.java
@@ -3,12 +3,28 @@ package org.as1iva.util;
 import lombok.experimental.UtilityClass;
 import org.as1iva.exception.InvalidDataException;
 
+import java.util.regex.Pattern;
+
 @UtilityClass
 public class ValidationUtil {
 
     public void checkUsername(String username) {
         if (username.length() <= 4) {
             throw new InvalidDataException("Username must be at least 5 characters long");
+        }
+    }
+
+    public void checkPath(String path) {
+        if (path == null) {
+            throw new InvalidDataException("Path is empty");
+        }
+
+        Pattern pattern = Pattern.compile(
+                "^([a-zA-Zа-яА-Я0-9_-][a-zA-Zа-яА-Я0-9._-]*/)*[a-zA-Zа-яА-Я0-9_-][a-zA-Zа-яА-Я0-9._-]*/?$"
+        );
+
+        if (!pattern.matcher(path).matches() && !path.isEmpty()) {
+            throw new InvalidDataException("Path is invalid");
         }
     }
 }
