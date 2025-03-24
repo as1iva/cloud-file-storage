8bbb6704fc009ab684410153f2f864a3890eaab2
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-24 14:20:07 +0300

    feat(service): add `getInfo` method in `DirectoryService`

--- src/main/java/org/as1iva/service/DirectoryService.java
+++ src/main/java/org/as1iva/service/DirectoryService.java
@@ -1,5 +1,7 @@
 package org.as1iva.service;
 
+import io.minio.Result;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import org.as1iva.dto.response.ResourceResponseDto;
 import org.as1iva.exception.DataExistsException;
@@ -9,6 +11,9 @@ import org.as1iva.util.PathUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -17,6 +22,54 @@ public class DirectoryService {
     private final MinioService minioService;
 
     private final String DIRECTORY_TYPE = "DIRECTORY";
+    private final String FILE_TYPE = "FILE";
+
+
+    public List<ResourceResponseDto> getInfo(String path, Long userId) {
+        String completePath = PathUtil.getUserPath(path, userId);
+
+        if (!path.isEmpty() && !isDirectoryExists(completePath)) {
+            throw new DataNotFoundException("Directory does not exist");
+        }
+
+        Iterable<Result<Item>> objects = minioService.getObjects(completePath);
+        List<ResourceResponseDto> resources = new ArrayList<>();
+
+        for (Result<Item> object : objects) {
+            try {
+                Item item = object.get();
+
+                if (item.objectName().equals(completePath)) {
+                    continue;
+                }
+
+                String objectName = item.objectName();
+                Long size = item.size();
+                String type = FILE_TYPE;
+                String name = PathUtil.getFileName(objectName);
+
+                if (item.isDir()) {
+                    size = null;
+                    type = DIRECTORY_TYPE;
+                    name = PathUtil.getDirectoryName(objectName);
+                }
+
+                ResourceResponseDto resource = ResourceResponseDto.builder()
+                        .path(path)
+                        .name(name)
+                        .size(size)
+                        .type(type)
+                        .build();
+
+                resources.add(resource);
+            } catch (Exception e) {
+                throw new InternalServerException();
+            }
+        }
+
+        return resources;
+    }
+
 
     public ResourceResponseDto createDirectory(String path, Long userId) {
         String completePath = PathUtil.getUserPath(path, userId);
