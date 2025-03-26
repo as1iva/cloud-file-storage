eb00b1ba2c5f3332d3f551557d80d0fe98703a5a
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-26 16:28:57 +0300

    feat(controller): add path validation in `FileController`

--- src/main/java/org/as1iva/controller/FileController.java
+++ src/main/java/org/as1iva/controller/FileController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.as1iva.dto.response.ResourceResponseDto;
 import org.as1iva.security.SecurityUserDetails;
 import org.as1iva.service.FileService;
+import org.as1iva.util.ValidationUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,8 @@ public class FileController {
     @GetMapping("/resource")
     public ResponseEntity<ResourceResponseDto> getInfo(@RequestParam("path") String path,
                                                        @AuthenticationPrincipal SecurityUserDetails userDetails) {
+
+        ValidationUtil.checkPath(path);
 
         return ResponseEntity.ok().body(fileService.getInfo(path, userDetails.getId()));
     }
