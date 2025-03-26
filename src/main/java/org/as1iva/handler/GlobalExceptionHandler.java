d9ec34c2ccd6138e3073ac86cb6dba123e47c724
Tim Fadeev <1mailforwork1@gmail.com>
2025-03-26 12:55:55 +0300

    refactor(handler): use `ErrorResponseDto` for responses instead of `Map` in `GlobalExceptionHandler`

--- src/main/java/org/as1iva/handler/GlobalExceptionHandler.java
+++ src/main/java/org/as1iva/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.as1iva.handler;
 
+import org.as1iva.dto.response.ErrorResponseDto;
 import org.as1iva.exception.DataExistsException;
 import org.as1iva.exception.DataNotFoundException;
 import org.as1iva.exception.InvalidDataException;
@@ -11,64 +12,62 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<?> handleDataIntegrityViolationException() {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of(
-                "status", 409,
-                "message", "Username is already taken"
-        ));
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponseDto.builder()
+                .status(409)
+                .message("Username is already taken")
+                .build());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> handleBadCredentialsException() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
-                "status", 401,
-                "message", "Password or username are incorrect"
-        ));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponseDto.builder()
+                .status(401)
+                .message("Password or username are incorrect")
+                .build());
     }
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<?> handleInvalidDataException(InvalidDataException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
-                "status", 400,
-                "message", e.getMessage()
-        ));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+                .status(400)
+                .message(e.getMessage())
+                .build());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException() {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
-                "status", 500,
-                "message", "Internal server error"
-        ));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponseDto.builder()
+                .status(500)
+                .message("Internal server error")
+                .build());
     }
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<?> handleDataNotFoundException(DataNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
-                "status", 404,
-                "message", e.getMessage()
-        ));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto.builder()
+                .status(404)
+                .message(e.getMessage())
+                .build());
     }
 
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<?> handleInternalServerException() {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
-                "status", 500,
-                "message", "Internal server error"
-        ));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponseDto.builder()
+                .status(500)
+                .message("Internal server error")
+                .build());
     }
 
     @ExceptionHandler(DataExistsException.class)
     public ResponseEntity<?> handleDataExistsException(DataExistsException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of(
-                "status", 409,
-                "message", e.getMessage()
-        ));
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponseDto.builder()
+                .status(409)
+                .message(e.getMessage())
+                .build());
     }
 }
