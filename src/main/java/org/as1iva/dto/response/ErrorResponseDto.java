67bd4552187c07f6a7a9db5b33c28b9fe1717227
Tim Fadeev <1mailforwork1@gmail.com>
2025-04-03 18:20:42 +0300

    refactor(dto): replace record with class for `ErrorResponseDto`

--- src/main/java/org/as1iva/dto/response/ErrorResponseDto.java
+++ src/main/java/org/as1iva/dto/response/ErrorResponseDto.java
@@ -1,8 +1,17 @@
 package org.as1iva.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
-public record ErrorResponseDto(int status, String message) {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
 
+    private int status;
+
+    private String message;
 }
